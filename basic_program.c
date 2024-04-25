d2789bf543ad0c0211cfaa584cf1bd40eaa0b2d3
Md Mahbub Hasan Talukder <mahbub.hasan@brainstation-23.com>
2024-04-25 11:21:07 +0600

    basic programming problem (factorial) added

--- basic_program.c
+++ basic_program.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+int getFactorial(int n){
+    int answer = 1;
+    for(int i=1;i<=n;i++)ans*=i;
+    return ans;
+}
 int main(){
-    printf("Hello world\n");
+    printf("%d",getFactorial(10));
 }
